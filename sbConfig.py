850ac48c03a8297f8e5c6f5e1be01063406c82de
Matthew <mbichay@csumb.edu>
2016-05-10 22:14:23 -0400

    Coded up most of the main python script interface. sbConfig contains the menues and options for interacting with the configuration tool. sbProfile is the data structure for storing vehicle data. Added experimental folder for scripts I'm using to prototype estimating the optimum shiftpoints.

---
+++ sbConfig.py
@@ -0,0 +1,121 @@
+import os
+import pickle
+from sbProfile import sbProfile
+
+
+def createNewProfile(selection):
+     
+    profile = sbProfile(input("> Profile Name: "))
+    profile.tireDiameter = parseNumericInput("> Tire Diameter (in): ", 'float')
+    profile.gearCount = parseNumericInput("> Gear Count: ", 'int')
+    for i in range(0, profile.gearCount):
+        profile.gearRatios.append(parseNumericInput("> [Gear " + str(i+1) + "] Overall Ratio: ", 'float'))
+   
+    if selection == '1':
+        for i in range(0, profile.gearCount): 
+            profile.shiftPoints.append(parseNumericInput("> [Gear " + str(i+1) +"] Shift Point (RPM): ", 'float'))
+    else:
+        pass
+
+    profile.earlyWarning = parseNumericInput("> Early Warning (RPM): ", 'float')
+    clear()
+
+    profile.summary()
+    
+    path = os.path.abspath(os.path.normpath(input("\n> Output Directory: ")))
+    save(profile, path)
+    clear()
+    print("[ SB Profile has been saved ]\n\n")
+
+def createNewProfileMenu():
+    createMenu = """\
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+|     Profile Creation Menu      |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+| 1. Create manually             |
+| 2. Create with TQ Curve        |
+| 0. Back                        |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+    options = {'0' : lambda x: None,
+               '1' : createNewProfile,
+               '2' : createNewProfile,
+    }
+    
+    selection = getMenuOption(createMenu, options)
+    options[selection](selection)
+    return selection
+
+
+def exportProfilesMenu():
+    return 0
+
+
+def getMenuOption(menu, options):
+    menu = menu + "\n> Please select a menu option: " 
+    selection = input(menu)
+    while selection not in options:
+        print("\nInvalid selection\n")
+        selection = input(menu)
+    clear()
+    return selection
+
+
+def parseNumericInput(prompt, dataType = 'int'):
+    
+    dataTypes = {'int' : lambda x: int(x),
+                 'float' : lambda x: float(x),
+    }
+    assert(dataType in dataTypes)
+    
+    valid = False
+    while not valid:
+         try:
+             userInput = dataTypes[dataType](input(prompt))
+             valid = True
+         except ValueError:
+             print("\nInvalid Input: Please enter a value of type " + dataType + "\n")
+             continue
+    return userInput
+
+def save(profile, path):
+    if not os.path.exists(path):
+        print("File Directory not found. Attempting to create.")
+        os.makedirs(path)
+    with open(os.path.join(path, profile.name + '.sb'), 'wb') as sbfile:
+        pickle.dump(profile, sbfile)
+
+   
+def mainMenu():
+    mainMenu = """\
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+|           Main Menu            |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+| 1. Create new profile          |
+| 2. Export existing profiles    |
+| 0. Exit                        |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    """
+    options = {'0' : lambda: None,
+               '1' : createNewProfileMenu,
+               '2' : exportProfilesMenu,
+    }
+
+    selection = getMenuOption(mainMenu,options)
+    options[selection]()
+    return selection
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def main():
+    clear()
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("| Shift Buddy Configuration Tool |")
+   
+    option = mainMenu()
+    while (option != '0'):
+        option = mainMenu()
+
+if __name__ == "__main__":
+    main()
---
+++ sbProfile.py
@@ -0,0 +1,34 @@
+
+class sbProfile:
+    def __init__(self, name=''):
+        self.name = name
+        self.tireDiameter = -1
+        self.gearCount = -1
+        self.gearRatios = []
+        self.shiftPoints = []
+        self.earlyWarning = -1
+
+    def isGood(self):
+        if (self.name != '' and
+            self.tireDiameter != -1 and
+            self.gearCount != -1 and
+            self.earlyWarning != -1 and
+            len(self.gearRatios) == self.gearCount and
+            len(self.shiftPoints) == self.gearCount
+            ):
+            return True
+        return False
+
+    def summary(self):
+        print("[ Profile Summary ]")
+        print("Name: " + self.name)
+        print("Tire Diameter (in): " + str(self.tireDiameter))
+        print("Gear Count: " + str(self.gearCount))
+        print("Gear Ratios: " + str(self.gearRatios))
+        print("Shift Points (RPM): " + str(self.shiftPoints))
+        print("Early Warning (RPM): " + str(self.earlyWarning))
+
+
+
+if __name__ == "__main__":
+    pass
